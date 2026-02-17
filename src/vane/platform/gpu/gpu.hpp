c78343e7ac897d0d53aff5b507f672c2caf41beb
mellic03 <mellic03@gmail.com>
2026-02-18 01:36:13 +1000

    wee

--- src/vane/platform/gpu/gpu.hpp
+++ src/vane/platform/gpu/gpu.hpp
@@ -3,7 +3,7 @@
 #ifdef VANE_GPU_OPENGL
     #include "opengl/gl.hpp"
 #elif defined(VANE_GPU_VULKAN)
-
+    #include "vulkan/vk.hpp"
 #else
     #error Must define VANE_GPU_OPENGL or VANE_GPU_VULKAN
 #endif
